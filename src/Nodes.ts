e1b6dcf78578cad6cefb0192cd0e8a104730af51
lmachens <leon@machens.koeln>
2023-03-21 13:02:49 +0100

    Add option to show/hide nodes

--- src/Nodes.ts
+++ src/Nodes.ts
@@ -4,6 +4,24 @@ import locations from "./lib/locations.json" assert { type: "json" };
 import { types } from "./lib/nodes";
 
 export default function Nodes({ map }: { map: leaflet.Map }) {
+  const group = new leaflet.LayerGroup();
+
+  const showNodes = document.querySelector<HTMLInputElement>("#show_nodes")!;
+  showNodes.checked = localStorage.getItem("hide_nodes") !== "true";
+  if (showNodes.checked) {
+    group.addTo(map);
+  }
+
+  showNodes.onchange = () => {
+    if (showNodes.checked) {
+      localStorage.removeItem("hide_nodes");
+      group.addTo(map);
+    } else {
+      localStorage.setItem("hide_nodes", "true");
+      group.removeFrom(map);
+    }
+  };
+
   locations.forEach((location) => {
     const type = types.find((type) => type.value === location.type)!;
 
@@ -11,7 +29,7 @@ export default function Nodes({ map }: { map: leaflet.Map }) {
       radius: 15,
       src: type.icon,
     });
-    marker.addTo(map);
+    marker.addTo(group);
     marker.bindTooltip(type.title, {
       direction: "top",
       offset: [0, -14],
