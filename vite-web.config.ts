a00208c7e3b861b45d96bfc30a745b32e883ab82
lmachens <leon@machens.koeln>
2023-04-04 12:26:58 +0200

    Add PWA

--- vite-web.config.ts
+++ vite-web.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,4 +16,43 @@ export default defineConfig({
   server: {
     port: 3034,
   },
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Sons Of The Forest Map",
+        short_name: "SOFT Map",
+        description:
+          "Stay on top of your game with real-time position tracking, nodes of weapons & points of interest, and overlay mode for seamless progress tracking.",
+        lang: "en",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webp}"],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
