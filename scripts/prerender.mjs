61f50e72386aa8893782aac2a46d651bd81c2551
lmachens <leon@machens.koeln>
2025-01-13 07:58:56 +0100

    No sandbox

--- scripts/prerender.mjs
+++ scripts/prerender.mjs
@@ -20,7 +20,9 @@ const server = await createServer({
 });
 await server.listen();
 
-const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({
+  args: ["--no-sandbox", "--disable-setuid-sandbox"],
+});
 const page = await browser.newPage();
 await page.setViewport({ width: 1200, height: 628 });
 
